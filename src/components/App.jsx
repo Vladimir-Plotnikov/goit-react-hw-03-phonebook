39619718f90a7fe26d3a8f9ce4915ee87a60a4f2
Volodymyr <vovdata@gmail.com>
2023-01-28 21:30:07 +0100

    yeet
    yeet23

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,7 +47,22 @@ deleteContact = contactId => {
     this.setState(({ contacts }) => ({
       contacts: contacts.filter(({ id }) => id !== contactId),
     }));
-  };
+};
+  
+componentDidMount() {
+  const contacts = localStorage.getItem('contacts');
+  const parsedContacts = JSON.parse(contacts);
+
+  if (parsedContacts) {
+    this.setState({contacts: parsedContacts})
+  }
+}
+  
+componentDidUpdate(prevProps, prevState) {
+if (this.state.contacts !== prevState.contacts) {
+  localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+}
+}
 
 render() {
   return (
